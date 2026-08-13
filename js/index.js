6c6dfb5283c9242673bf6d5dcfb71c93c47fa458
Mariano Inostroza <mariano.f.inostroza@gmail.com>
2026-08-13 14:29:19 -0400

    se crean layout de modales, botones básicos activos y se instala drag and drop

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/* -----------Selector de items------------- */
+
+let items = document.querySelector('.items')
+
+new Sortable(items, {
+    animation: 200,
+    handle: '.moveHand',
+    ghostClass: 'drag'
+});
+
+
+
+/* -----------Selector de items------------- */
+
